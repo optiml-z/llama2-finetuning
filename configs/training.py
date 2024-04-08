5d4aa6f3ec69b1058d823d2b02829f338ab249a8
root <lionelbishwash11@gmail.com>
2024-04-08 09:21:07 +0000

    added extra useful modules from llama-recipr

--- configs/training.py
+++ configs/training.py
@@ -7,25 +7,24 @@ from dataclasses import dataclass
 @dataclass
 class train_config:
     model_name: str="PATH/to/LLAMA/7B"
-    tokenizer_path :str = ""
     enable_fsdp: bool=False
     low_cpu_fsdp: bool=False
     run_validation: bool=True
-    batch_size_training: int=4
+    batch_size_training: int=8
     batching_strategy: str="packing" #alternative: padding
     context_length: int=4096
     gradient_accumulation_steps: int=1
     gradient_clipping: bool = False
     gradient_clipping_threshold: float = 1.0
-    num_epochs: int=3
-    num_workers_dataloader: int=1
+    num_epochs: int=10
+    num_workers_dataloader: int=16
     lr: float=1e-4
-    weight_decay: float=0.0
+    weight_decay: float=0.01
     gamma: float= 0.85
     seed: int=42
-    use_fp16: bool=False
+    use_fp16: bool=True
     mixed_precision: bool=True
-    val_batch_size: int=1
+    val_batch_size: int=4
     dataset = "samsum_dataset"
     peft_method: str = "lora" # None , llama_adapter, prefix
     use_peft: bool=False
@@ -37,7 +36,7 @@ class train_config:
     save_model: bool = True
     dist_checkpoint_root_folder: str="PATH/to/save/FSDP/model" # will be used if using FSDP
     dist_checkpoint_folder: str="fine-tuned" # will be used if using FSDP
-    save_optimizer: bool=False # will be used if using FSDP
-    use_fast_kernels: bool = False # Enable using SDPA from PyTorch Accelerated Transformers, make use Flash Attention and Xformer memory-efficient kernels
+    save_optimizer: bool=True # will be used if using FSDP
+    use_fast_kernels: bool = True # Enable using SDPA from PyTroch Accelerated Transformers, make use Flash Attention and Xformer memory-efficient kernels
     use_wandb: bool = False # Enable wandb for experient tracking
     save_metrics: bool = False # saves training metrics to a json file for later plotting
