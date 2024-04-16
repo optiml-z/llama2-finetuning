554fdbbbadf404e350d66254b4709eddaa47083a
Bishwash Khanal <lionelbishwash11@gmail.com>
2024-04-16 10:43:49 +0545

    modified path for alpaca dataset

--- configs/datasets.py
+++ configs/datasets.py
@@ -5,12 +5,12 @@ class alpaca_dataset:
     dataset: str = "alpaca_dataset"
     train_split: str = "train"
     test_split: str = "val"
-    data_path: str = "/root/workspace/llama2_finetuning/datasets/alpaca/alpaca_dataset.json"
+    data_path: str = "datasets/alpaca/alpaca_gpt4_data.json"
     
     
 @dataclass
 class custom_dataset:
     dataset: str = "custom_dataset"
-    file: str = "examples/custom_dataset.py"
+    file: str = "dataset/custom_dataset.py"
     train_split: str = "train"
     test_split: str = "validation"
